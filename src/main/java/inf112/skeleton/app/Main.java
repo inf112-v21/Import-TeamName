b18fa4921b896ce143c9376156c8e2ceb341685c
Endre <Endre.Tonnessen@student.uib.no>
2021-02-09 13:12:32 +0100

    Changed which game is launched.

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -11,10 +11,11 @@ public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("ROBORALLY");
-        cfg.setWindowedMode(2560, 1440);
+        cfg.setWindowedMode(500,500);
 
-        HelloWorld helloWorld = new HelloWorld();
-        new Lwjgl3Application(helloWorld, cfg);
+        //HelloWorld helloWorld = new HelloWorld();
+        TestGame game = new TestGame();
+        new Lwjgl3Application(game, cfg);
 
     }
 }
