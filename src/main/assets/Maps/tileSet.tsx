fa56dda6f9f1b5f867d215145e318355e21c754f
Sondre Eide <sondre.eide@gmail.com>
2021-02-17 15:16:39 +0100

    Moved assets

--- src/main/assets/Maps/tileSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tileset" tilewidth="300" tileheight="300" tilecount="136" columns="8">
- <image source="../Images/tiles.png" width="2400" height="5100"/>
-</tileset>
