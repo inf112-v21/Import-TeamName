447125cf82be85c4eb064cde3fe69b370d3845d6
Endre <Endre.Tonnessen@student.uib.no>
2021-02-19 22:07:41 +0100

    Removed inheritance relation between IActor and IObject

--- src/main/java/inf112/skeleton/app/objects/IActor.java
+++ src/main/java/inf112/skeleton/app/objects/IActor.java
@@ -1,11 +1,31 @@
 package inf112.skeleton.app.objects;
 
+import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
 
-public interface IActor extends IObject {
+public interface IActor {
 
+    /**
+     *
+     * @return Position of object on board.
+     */
+    public Vector2 getPosition();
+
+    /**
+     *  Sets an objects position on the board.
+     * @param position
+     */
+    public void setPosition(Vector2 position);
+
+    /**
+     * @return Directions actor is looking.
+     */
     Direction getLookDirection();
 
+    /**
+     * Change direction actor is looking.
+     * @param direction
+     */
     void setLookDirection(Direction direction);
 
 }
