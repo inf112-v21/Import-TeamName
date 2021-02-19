7fba6e63adae0784852705f776ad8d209134b79f
Endre <Endre.Tonnessen@student.uib.no>
2021-02-19 22:42:09 +0100

    added function returning the opposite direction.

--- src/main/java/inf112/skeleton/app/enums/Direction.java
+++ src/main/java/inf112/skeleton/app/enums/Direction.java
@@ -8,6 +8,24 @@ public enum Direction {
     NORTH,
     EAST,
     SOUTH,
-    WEST
+    WEST;
+
+    /**
+     * Returns opposite direction
+     * @param dir
+     * @return
+     */
+    public static Direction DirectionOpposite(Direction dir) {
+        if (!(dir instanceof Direction)) throw new IllegalArgumentException("Expected enum direction.");
+
+        switch (dir) {
+            case NORTH: return Direction.SOUTH;
+            case SOUTH: return Direction.NORTH;
+            case EAST: return Direction.WEST;
+            case WEST: return Direction.EAST;
+        }
+        return null;
+    }
+
 
 }
