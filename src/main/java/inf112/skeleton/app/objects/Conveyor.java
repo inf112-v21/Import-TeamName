565b58ed5fb9ba4b672301057362da02582b6654
Endre <Endre.Tonnessen@student.uib.no>
2021-02-19 21:37:24 +0100

    Added constructor method. Added speed variable.

--- src/main/java/inf112/skeleton/app/objects/Conveyor.java
+++ src/main/java/inf112/skeleton/app/objects/Conveyor.java
@@ -3,10 +3,17 @@ package inf112.skeleton.app.objects;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
 
-public class Conveyor implements Object {
+public class Conveyor implements IObject {
 
     private Vector2 position;
     private Direction pushDirection;
+    private Integer speed; // Normal push 1 tile, Fast, push 2 tiles in 2 turns
+
+    public Conveyor(Vector2 position, Direction pushDirection, Integer speed){
+        this.position = position;
+        this.pushDirection = pushDirection;
+        this.speed = speed;
+    }
 
     @Override
     public Vector2 getPosition() {
