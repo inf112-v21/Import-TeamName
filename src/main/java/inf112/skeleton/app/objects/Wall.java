5183ce017ce0e584804c230d377f911df703c749
Endre <Endre.Tonnessen@student.uib.no>
2021-02-19 21:49:32 +0100

    Added wall.java

---
+++ src/main/java/inf112/skeleton/app/objects/Wall.java
@@ -0,0 +1,25 @@
+package inf112.skeleton.app.objects;
+
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.enums.Direction;
+
+public class Wall implements IObject{
+
+    private final Vector2 position;
+    private final Direction direction; // Direction of wall in a tile? If direction=NORTH, then cannot go north when standing on tile?
+
+    public Wall(Vector2 position, Direction direction) {
+        this.position = position;
+        this.direction = direction;
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return null;
+    }
+
+    @Override
+    public void setPosition(Vector2 position) {
+
+    }
+}
