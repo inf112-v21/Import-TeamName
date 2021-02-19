c467bb5b81ef5dd39e6882c1c5a6df703198bca2
August Klevberg <august.klevberg@student.uib.no>
2021-02-19 15:45:48 +0100

    Laser class

--- src/main/java/inf112/skeleton/app/objects/Laser.java
+++ src/main/java/inf112/skeleton/app/objects/Laser.java
@@ -1,19 +1,25 @@
 package inf112.skeleton.app.objects;
 
 import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.enums.Direction;
 
 public class Laser implements Object {
 
     private Vector2 position;
+    private Direction direction;
 
     @Override
     public Vector2 getPosition() {
-        return position;
+        return this.position;
     }
 
     @Override
-    public void setPosition(Vector2 position) {
+    public void setPosition(Vector2 pos) {
+        this.position = pos;
+    }
 
+    public Direction getDirection() {
+        return this.direction;
     }
 
 }
