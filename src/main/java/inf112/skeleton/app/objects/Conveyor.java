60250af2dcfb02829f4c9b5bb6ce4356dbe8f86d
Endre <Endre.Tonnessen@student.uib.no>
2021-02-19 22:21:55 +0100

    Added getter funk for speed, changed speed to final.

--- src/main/java/inf112/skeleton/app/objects/Conveyor.java
+++ src/main/java/inf112/skeleton/app/objects/Conveyor.java
@@ -6,8 +6,8 @@ import inf112.skeleton.app.enums.Direction;
 public class Conveyor implements IObject {
 
     private Vector2 position;
-    private Direction pushDirection;
-    private Integer speed; // Normal push 1 tile, Fast, push 2 tiles in 2 turns
+    private final Direction pushDirection;
+    private final Integer speed; // Normal push 1 tile, Fast, push 2 tiles in 2 turns
 
     public Conveyor(Vector2 position, Direction pushDirection, Integer speed){
         this.position = position;
@@ -25,13 +25,17 @@ public class Conveyor implements IObject {
         this.position = pos;
     }
 
-    public Direction getPushDirection() {
-        return this.pushDirection;
-    }
-
     @Override
     public boolean isPassableFromDirection(Direction dir) {
         return true;
     }
 
+
+    public Direction getPushDirection() {
+        return this.pushDirection;
+    }
+
+    public Integer getSpeed() {
+        return speed;
+    }
 }
