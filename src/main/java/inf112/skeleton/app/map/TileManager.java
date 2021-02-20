02d5e8ab7f03ff62d7309427ad403267a5e7a632
Endre <Endre.Tonnessen@student.uib.no>
2021-02-20 09:39:26 +0100

    Added TileManager.java class.

---
+++ src/main/java/inf112/skeleton/app/map/TileManager.java
@@ -0,0 +1,15 @@
+package inf112.skeleton.app.map;
+
+/**
+ *    Creates an instance of all tiles on the board.
+ */
+public class TileManager {
+
+    /*
+         Plan:
+             - Loop through all tiles on board.
+             - Create class instance of tile, with correct direction and other data,
+             - Return a list of all tiles? 1D og 2D list? Represent board?
+
+     */
+}
