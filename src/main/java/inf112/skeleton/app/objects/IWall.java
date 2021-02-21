07895f2b9e33595dc7cfda5f2c97614aca0b849f
Endre <Endre.Tonnessen@student.uib.no>
2021-02-21 21:20:10 +0100

    Added comments.

--- src/main/java/inf112/skeleton/app/objects/IWall.java
+++ src/main/java/inf112/skeleton/app/objects/IWall.java
@@ -6,16 +6,16 @@ public interface IWall extends IObject {
 
     /**
      * Checks if an actor can move away from its own tile in a given direction.
-     * @return true if solid, cannot pass through it.
+     * @return true if can move.
      */
     boolean isPassableFromDirection(Direction dir);
 
 
     /**
      * Checks if actor can move onto another tile from a given direction.
-     * Note: This function may not be necessary.
+     * Note: This function may not be necessary, as we can call the above function on any tile we want to move to.
      * @param dir
-     * @return
+     * @return True if can move.
      */
     boolean isPassableToDirection(Direction dir);
 
