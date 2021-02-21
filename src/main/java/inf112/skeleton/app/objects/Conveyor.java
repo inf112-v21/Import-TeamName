76d8e548b6e4787dbb5571d30a9f344ea465a389
Endre <Endre.Tonnessen@student.uib.no>
2021-02-21 21:05:43 +0100

    Changed Coveyor.java to inherit from object.java

--- src/main/java/inf112/skeleton/app/objects/Conveyor.java
+++ src/main/java/inf112/skeleton/app/objects/Conveyor.java
@@ -3,28 +3,19 @@ package inf112.skeleton.app.objects;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
 
-public class Conveyor implements IObject {
+/**
+ *  Class for conveyors
+ */
+public class Conveyor extends Object {
 
-    private Vector2 position;
     private final Direction pushDirection;
     private final Integer speed; // Normal push 1 tile, Fast, push 2 tiles in 2 turns
 
     public Conveyor(Vector2 position, Direction pushDirection, Integer speed){
-        this.position = position;
+        super(position);
         this.pushDirection = pushDirection;
         this.speed = speed;
     }
-
-    @Override
-    public Vector2 getPosition() {
-        return this.position;
-    }
-
-    @Override
-    public void setPosition(Vector2 pos) {
-        this.position = pos;
-    }
-
 
     public Direction getPushDirection() {
         return this.pushDirection;
