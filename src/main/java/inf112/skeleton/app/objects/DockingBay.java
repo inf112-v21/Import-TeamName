d548fcaa47bee75b84b6a3086c07ba764c81c425
Endre <Endre.Tonnessen@student.uib.no>
2021-02-21 21:04:11 +0100

    added DockingBay.java. (Staring points of all robots in the game.)

---
+++ src/main/java/inf112/skeleton/app/objects/DockingBay.java
@@ -0,0 +1,13 @@
+package inf112.skeleton.app.objects;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ *  Class for the staring point of robots.
+ */
+public class DockingBay extends Object{
+
+    public DockingBay(Vector2 position) {
+        super(position);
+    }
+}
