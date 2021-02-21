1364f596479d42c045a1f1cfda59d3f25753356c
Endre <Endre.Tonnessen@student.uib.no>
2021-02-21 21:11:59 +0100

    Changed Laser.java to inherit from object.java.

--- src/main/java/inf112/skeleton/app/objects/Laser.java
+++ src/main/java/inf112/skeleton/app/objects/Laser.java
@@ -3,25 +3,14 @@ package inf112.skeleton.app.objects;
 import com.badlogic.gdx.math.Vector2;
 import inf112.skeleton.app.enums.Direction;
 
-public class Laser implements IWall {
+public class Laser extends Object implements IWall {
 
-    private Vector2 position;
     //Direction of laser is where the laser ends. Laser(SOUTH), has a wall to its NORTH.
     private final Direction direction; // Made final, will direction of laser/wall ever change?
 
     public Laser(Vector2 position, Direction direction) {
-        this.position = position;
+        super(position);
         this.direction = direction;
-    }
-
-    @Override
-    public Vector2 getPosition() {
-        return this.position;
-    }
-
-    @Override
-    public void setPosition(Vector2 pos) {
-        this.position = pos;
     }
 
     public Direction getDirection() {
