d6b866d54fdda7a70bb59a4a98e1f0e37a05196f
Endre-Tonnessen <endres@lyse.net>
2021-02-22 17:52:46 +0100

    Can now differentiate between single and double repair site.

--- src/main/java/inf112/skeleton/app/objects/TileObjects/RepairSite.java
+++ src/main/java/inf112/skeleton/app/objects/TileObjects/RepairSite.java
@@ -5,8 +5,15 @@ import inf112.skeleton.app.objects.SimpleObject;
 
 public class RepairSite extends SimpleObject {
 
-    public RepairSite(Vector2 position) {
+    //Change name maybe? Can be 1 or 2. Single or Double.
+    private int strength;
+
+    public RepairSite(Vector2 position, int strength) {
         super(position);
+        this.strength = strength;
     }
 
+    public int getStrength() {
+        return strength;
+    }
 }
