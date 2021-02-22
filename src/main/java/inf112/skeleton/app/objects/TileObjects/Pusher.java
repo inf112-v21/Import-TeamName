985781206a9c5a63447bad71f9d8bf659e50f3a8
Endre <Endre.Tonnessen@student.uib.no>
2021-02-22 13:05:05 +0100

    added content to Pusher.java

--- src/main/java/inf112/skeleton/app/objects/TileObjects/Pusher.java
+++ src/main/java/inf112/skeleton/app/objects/TileObjects/Pusher.java
@@ -1,4 +1,26 @@
 package inf112.skeleton.app.objects.TileObjects;
 
-public class Pusher {
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.enums.Direction;
+import inf112.skeleton.app.objects.IWall;
+import inf112.skeleton.app.objects.Object;
+
+public class Pusher extends Object implements IWall{
+
+    private final Direction pushDirection;
+
+    public Pusher(Vector2 position, Direction pushDirection) {
+        super(position);
+        this.pushDirection = pushDirection;
+    }
+
+    @Override
+    public boolean isPassableFromDirection(Direction dir) {
+        return dir != Direction.DirectionOpposite(dir); //Wall is opposite to pushDirection.
+    }
+
+    @Override
+    public boolean isPassableToDirection(Direction dir) {
+        return false;
+    }
 }
