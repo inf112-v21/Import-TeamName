14f87443e53b7e211a94a4823b4025840770787a
Endre-Tonnessen <endres@lyse.net>
2021-02-22 17:46:39 +0100

    Added Pit.java.

---
+++ src/main/java/inf112/skeleton/app/objects/TileObjects/Pit.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app.objects.TileObjects;
+
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.objects.SimpleObject;
+
+public class Pit extends SimpleObject {
+
+    public Pit(Vector2 position) {
+        super(position);
+    }
+}
