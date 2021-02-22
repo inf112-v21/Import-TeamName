568d1780c770eb9429566622f62ec83b269b0b04
Endre-Tonnessen <endres@lyse.net>
2021-02-22 17:59:27 +0100

    Expanded to include rotation.

--- src/main/java/inf112/skeleton/app/objects/TileObjects/Gear.java
+++ src/main/java/inf112/skeleton/app/objects/TileObjects/Gear.java
@@ -1,12 +1,19 @@
 package inf112.skeleton.app.objects.TileObjects;
 
 import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.app.enums.Direction;
 import inf112.skeleton.app.objects.SimpleObject;
 
 public class Gear extends SimpleObject {
 
-    public Gear(Vector2 position) {
+    private Direction turnDirection;
+
+    public Gear(Vector2 position, Direction turnDirection) {
         super(position);
+        this.turnDirection = turnDirection;
     }
 
+    public Direction getTurnDirection() {
+        return turnDirection;
+    }
 }
