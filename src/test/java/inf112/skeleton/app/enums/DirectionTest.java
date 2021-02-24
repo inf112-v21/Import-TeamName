37c1eeb7d3fbeb9627bb1132e31841bdce7061b8
Endre-Tonnessen <endres@lyse.net>
2021-02-24 15:37:27 +0100

    added more tests to Direction.java

--- src/test/java/inf112/skeleton/app/enums/DirectionTest.java
+++ src/test/java/inf112/skeleton/app/enums/DirectionTest.java
@@ -5,9 +5,6 @@ import org.junit.*;
 
 import static org.junit.Assert.fail;
 
-/**
- * Tests funcitons in Direction.java.
- */
 public class DirectionTest {
 
     @Test
@@ -28,6 +25,39 @@ public class DirectionTest {
     @Test
     public void DirectionOppositeShouldReturnWest() {
         Assert.assertEquals(Direction.WEST, Direction.DirectionOpposite(Direction.EAST));
+    }
+
+
+    
+
+    @Test
+    public void goDirectionShouldReturnCorrectVectorWest() {
+        Vector2 westPos = Direction.goDirection(new Vector2(0,0), Direction.WEST);
+        Assert.assertEquals(new Vector2(-1,0), westPos);
+    }
+
+    @Test
+    public void goDirectionShouldReturnCorrectVectorEast() {
+        Vector2 eastPos = Direction.goDirection(new Vector2(0,0), Direction.EAST);
+        Assert.assertEquals(new Vector2(1,0), eastPos);
+    }
+
+    @Test
+    public void goDirectionShouldReturnCorrectVectorNorth() {
+        Vector2 northPos = Direction.goDirection(new Vector2(0,0), Direction.NORTH);
+        Assert.assertEquals(new Vector2(0,1), northPos);
+    }
+
+    @Test
+    public void goDirectionShouldReturnCorrectVectorSouth() {
+        Vector2 southPos = Direction.goDirection(new Vector2(0,0), Direction.SOUTH);
+        Assert.assertEquals(new Vector2(0,-1), southPos);
+    }
+
+    @Test
+    public void goDirectionShouldReturnIncorrectVectorNorth() {
+        Vector2 northPos = Direction.goDirection(new Vector2(0,0), Direction.NORTH);
+        Assert.assertNotEquals(new Vector2(1,1), northPos);
     }
 
     @Test
