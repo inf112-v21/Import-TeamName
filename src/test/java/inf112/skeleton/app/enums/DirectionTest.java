51a6292ce18d6d359c33bfb75aeadd0b5e01f990
Endre-Tonnessen <endres@lyse.net>
2021-02-24 15:13:57 +0100

    added tests to Direction.java

--- src/test/java/inf112/skeleton/app/enums/DirectionTest.java
+++ src/test/java/inf112/skeleton/app/enums/DirectionTest.java
@@ -1,9 +1,42 @@
 package inf112.skeleton.app.enums;
 
-import org.junit.Test;
+import com.badlogic.gdx.math.Vector2;
+import org.junit.*;
 
+import static org.junit.Assert.fail;
+
+/**
+ * Tests funcitons in Direction.java.
+ */
 public class DirectionTest {
 
     @Test
+    public void DirectionOppositeShouldReturnNorth() {
+        Assert.assertEquals(Direction.NORTH, Direction.DirectionOpposite(Direction.SOUTH));
+    }
 
+    @Test
+    public void DirectionOppositeShouldReturnSouth() {
+        Assert.assertEquals(Direction.SOUTH, Direction.DirectionOpposite(Direction.NORTH));
+    }
+
+    @Test
+    public void DirectionOppositeShouldReturnEast() {
+        Assert.assertEquals(Direction.EAST, Direction.DirectionOpposite(Direction.WEST));
+    }
+
+    @Test
+    public void DirectionOppositeShouldReturnWest() {
+        Assert.assertEquals(Direction.WEST, Direction.DirectionOpposite(Direction.EAST));
+    }
+
+    @Test
+    public void goDirectionShouldFailWhenGivenRotation() {
+        try {
+            Direction.goDirection(new Vector2(2,2), Direction.AGAINST_CLOCK);
+            fail();
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
 }
