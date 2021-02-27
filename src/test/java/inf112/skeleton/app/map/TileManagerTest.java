22d268379a7bcf64e811aa56ce23e75d070bdcda
Endre-Tonnessen <endres@lyse.net>
2021-02-27 17:39:06 +0100

    Added more tests to cover each tile object type. TileMangager.java

--- src/test/java/inf112/skeleton/app/map/TileManagerTest.java
+++ src/test/java/inf112/skeleton/app/map/TileManagerTest.java
@@ -1,10 +1,7 @@
 package inf112.skeleton.app.map;
 
 import com.badlogic.gdx.math.Vector2;
-import inf112.skeleton.app.objects.TileObjects.Laser;
-import inf112.skeleton.app.objects.TileObjects.Pit;
-import inf112.skeleton.app.objects.TileObjects.Pusher;
-import inf112.skeleton.app.objects.TileObjects.Wall;
+import inf112.skeleton.app.objects.TileObjects.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -37,5 +34,25 @@ public class TileManagerTest {
     @Test
     public void getTileShouldReturnPitObject() {
         Assert.assertTrue(tileManager.getTileObject(6, pos) instanceof Pit);
+    }
+
+    @Test
+    public void getTileShouldReturnFlagObject() {
+        Assert.assertTrue(tileManager.getTileObject(55, pos) instanceof Flag);
+    }
+
+    @Test
+    public void getTileShouldReturnGearObject() {
+        Assert.assertTrue(tileManager.getTileObject(53, pos) instanceof Gear);
+    }
+
+    @Test
+    public void getTileShouldReturnConveyorObject() {
+        Assert.assertTrue(tileManager.getTileObject(22, pos) instanceof Conveyor);
+    }
+
+    @Test
+    public void getTileShouldReturnRepairSiteObject() {
+        Assert.assertTrue(tileManager.getTileObject(15, pos) instanceof RepairSite);
     }
 }
