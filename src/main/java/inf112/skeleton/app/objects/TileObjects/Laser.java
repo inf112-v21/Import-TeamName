9088300b0b578df8c831c7cb2daf863df29fe4c9
Endre-Tonnessen <endres@lyse.net>
2021-02-27 17:29:20 +0100

    Added strength variable to Laser.java. Used for Single and Double Laser objects.

--- src/main/java/inf112/skeleton/app/objects/TileObjects/Laser.java
+++ src/main/java/inf112/skeleton/app/objects/TileObjects/Laser.java
@@ -9,15 +9,19 @@ public class Laser extends SimpleObject implements IWall {
 
     //Direction of laser is where the laser ends. Laser(SOUTH), has a wall to its NORTH.
     private final Direction direction; // Made final, will direction of laser/wall ever change?
+    private final int strength; // Single or Double laser
 
-    public Laser(Vector2 position, Direction direction) {
+    public Laser(Vector2 position, Direction direction, int strength) {
         super(position);
         this.direction = direction;
+        this.strength = strength;
     }
 
     public Direction getDirection() {
         return this.direction;
     }
+
+    public int getStrength() { return strength; }
 
     @Override
     public boolean isPassableFromDirection(Direction dir) {
