86b61f55b3333ec8acf143c5d3d90c6bc0cc6f43
Endre-Tonnessen <endres@lyse.net>
2021-03-15 21:02:00 +0100

    Added outline to test class for CompletRefisterTest

--- src/test/java/inf112/skeleton/app/game/CompleteRegisterPhaseTest.java
+++ src/test/java/inf112/skeleton/app/game/CompleteRegisterPhaseTest.java
@@ -1,4 +1,51 @@
 package inf112.skeleton.app.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import inf112.skeleton.app.GdxTestRunner;
+import inf112.skeleton.app.assetManager.Assets;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
 public class CompleteRegisterPhaseTest {
+
+
+    // Make maps
+    private TiledMap map;
+
+    private TextureRegion[][] textures;
+    private TiledMapTileLayer tilePlayer;
+    private MainGame game;
+
+    @Before
+    public void initalise() {
+        textures = new TextureRegion(new Texture("Images/player.png")).split(300, 300);  // Splits player texture into the 3 parts. Live/Dead/Win
+        map = new TmxMapLoader().load("Maps/MapForJunitTests.tmx");       // Get map file
+        tilePlayer = (TiledMapTileLayer) map.getLayers().get("Player");
+
+        Assets.load();
+        Assets.manager.finishLoading();
+
+        game = new MainGame();
+        game.setup(map);
+
+        CompleteRegisterPhase phase = new CompleteRegisterPhase();
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(true,true);
+    }
+
+
+
+
+
+
 }
