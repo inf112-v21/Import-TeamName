c269f97280f41f7bad112b92fbe89b6dc2daa73e
Klevberg <augustkl@hotmail.com>
2021-04-21 14:10:13 +0200

    Formatting RRServer

--- src/main/java/inf112/skeleton/app/multiplayer/RRServer.java
+++ src/main/java/inf112/skeleton/app/multiplayer/RRServer.java
@@ -20,64 +20,58 @@ import java.util.List;
 
 public class RRServer {
 
-    //Server Object
+    // Server Object
     Server server;
     MainGame mainGame;
     private TiledMap map;
 
 
-
     public RRServer(MainGame mainGame) throws IOException {
         this.mainGame = mainGame;
-        //Log.set(Log.LEVEL_DEBUG);
+        // Log.set(Log.LEVEL_DEBUG);
 
-        server = new Server() {
-            protected Connection newConnection() {  //Storing by connection state
+        this.server = new Server() {
+            protected Connection newConnection() {  // Storing by connection state
                 return new RRConnection();
             }
         };
 
-        //Bind to port
-        server.bind(NetworkPackets.tcpPort, NetworkPackets.udpPort);
+        // Bind to port
+        this.server.bind(NetworkPackets.tcpPort, NetworkPackets.udpPort);
 
-        server.start();
+        this.server.start();
         this.map = new TmxMapLoader().load("Maps/Chess.tmx");
-        mainGame.setup(map);
+        mainGame.setup(this.map);
 
+        // Registering of packet class is being done all at once at NetworkPackets.java
+        NetworkPackets.register(this.server);
 
-
-        //Registering of packet class is being done all at once at NetworkPackets.java
-        NetworkPackets.register(server);
-
-
-
-
-        server.addListener(new Listener() {
+        this.server.addListener(new Listener() {
             public void received(Connection c, Object packet) {
-                RRConnection connection = (RRConnection) c; //every connection is of RRConnection, so this should be fine.
+                RRConnection connection = (RRConnection) c; // Every connection is of RRConnection, so this should be fine.
 
                 if (packet instanceof Entry) {
-                    Entry type = ((Entry) packet); //casting to access the packet
+                    Entry type = ((Entry) packet); // Casting to access the packet
 
-                    //from here on, im just trying to catch invalid names
+                    // From here on, im just trying to catch invalid names
                     if (connection.name != null) return;
 
-                    String named = type.name; //since Entry might get other values in the future.
+                    String named = type.name; // Since Entry might get other values in the future.
                     if (named == null) return;
 
                     named = named.trim();
                     if (named.length() == 0) return;
 
-                    connection.name = named; //should be a valid name by this point.
+                    connection.name = named; // Should be a valid name by this point.
 
                     NetworkPackets.NewPlayer message = new NetworkPackets.NewPlayer(connection.name, connection.getID(), true);
                     server.sendToAllTCP(message);
 
                     for (Connection connections: server.getConnections()) {
-                        RRConnection rrConnection = (RRConnection) connections; //casting just in case it uses default connection instead
+                        RRConnection rrConnection = (RRConnection) connections; // Casting just in case it uses default connection instead
 
-                        //this here, had problems with adding players, at times it overlapped players at same spot
-                        //this "if" checks if it's yourself who is logging in. thus stopping ^ probably...
+                        // This here, had problems with adding players, at times it overlapped players at same spot
+                        // This "if" checks if it's yourself who is logging in. thus stopping ^ probably...
                         if (rrConnection.getID() != connection.getID() && rrConnection.name != null) {
                             NetworkPackets.NewPlayer message2 = new NetworkPackets.NewPlayer(rrConnection.name, rrConnection.getID(), true);
                             connection.sendTCP(message2);
@@ -96,7 +90,7 @@ public class RRServer {
                         }
                     }
  */
-               } //if a packet of unknown class gets sent from client somehow.
+               } // If a packet of unknown class gets sent from client somehow.
                 else if (!(packet instanceof FrameworkMessage.Ping) && !(packet instanceof FrameworkMessage.KeepAlive)) {
                     Log.info("An unknown packet was sent from Client.");
                 }
@@ -106,7 +100,7 @@ public class RRServer {
                 public void disconnected (Connection c) {
                     RRConnection connection = (RRConnection) c;
                     if (connection.name != null) {
-                        System.out.println(connection.name + " has disconnected."); //testing
+                        System.out.println(connection.name + " has disconnected."); // Testing
                         NetworkPackets.NewPlayer leaveMessage = new NetworkPackets.NewPlayer(connection.name, connection.getID(), false);
                         server.sendToAllTCP(leaveMessage);
                     }
@@ -120,8 +114,8 @@ public class RRServer {
     }
 
     public void ceaseServer() {
-        server.close();
-        server.stop();
+        this.server.close();
+        this.server.stop();
     }
 
     public TiledMap getMap() {
